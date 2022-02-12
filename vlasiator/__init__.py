4c49f7711ab8a4c7bde2cdc438ec43dbd3cffc44
Hongyang Zhou <hyzhou@umich.edu>
2022-02-12 16:01:28 +0200

    Initial commit

---
+++ vlasiator/__init__.py
@@ -0,0 +1,15 @@
+# type: ignore[attr-defined]
+"""Python interface for processing Vlasiator data"""
+
+import sys
+from importlib import metadata as importlib_metadata
+
+
+def get_version() -> str:
+    try:
+        return importlib_metadata.version(__name__)
+    except importlib_metadata.PackageNotFoundError:  # pragma: no cover
+        return "unknown"
+
+
+version: str = get_version()
